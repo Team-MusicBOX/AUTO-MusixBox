5f9b9051756d4b2daf9bfe0fb3d5c6d85e69d0c0
woojeongha <80499047+woojeongha@users.noreply.github.com>
2021-08-27 21:09:23 +0900

    Add files via upload

--- funcions.py
+++ funcions.py
@@ -8,16 +8,17 @@ def binary_img(img):
     return img
 
 def slice(original):
-    x = 37;
-    y = 62;
-    w = 12;
+    x = 37
+    y = 62
+    w = 12
     h = 17
     img = []
     roi = []
 
-    for i in range(0,4+1):
+    for i in range(0,30+1):
         roi.append(original[y:y+h, x:x+w])
         img.append(roi[i].copy())
-        rec = cv2.rectangle(original[i], (0, 0), (w - 1, h - 1), (255, 0, 0))
+        rec = cv2.rectangle(original, (x, y), (x+w, y+h), (255, 0, 0))
         x = x + 11
+
     return img, rec
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ music_pic = cv2.resize(music_pic,dsize=(400,400),interpolation=cv2.INTER_CUBIC)
 h, w, c = music_pic.shape
 ryan_img = []
 ain = 0
+
 if music_pic is not None:
     cv2.imshow("music", music_pic)
     if cv2.waitKey(0) == ord('c'):
@@ -17,10 +18,10 @@ if music_pic is not None:
 
 for i in img:
 
-    cv2.imshow("hdgg"+ str(ain),i)
-    cv2.moveWindow("hdgg" + str(ain),100 * ain ,400)
-
+    cv2.imshow("piece"+ str(ain),i)
+    cv2.moveWindow("piece" + str(ain) ,30 * ain ,400)
     ain = ain + 1
+
 cv2.imshow("god ",rec)
 cv2.waitKey(0)
 

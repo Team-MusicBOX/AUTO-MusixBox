18ddf5ac7bbdc3e732ca9ea7f3efea1d2ce197a7
woojeongha <80499047+woojeongha@users.noreply.github.com>
2021-09-02 15:49:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-# -해야할 것-
-# 코드 함수화(모듈화) -> main에 최대한 코드 줄이기
-# 라벨링을 통해서 검출해낸다음에 해당 영역을 30으로 나누기
-
-# --지금 할것--
-# 연주 해보기
-
 import cv2
 import numpy as np
-import switch as switch
 from multiprocessing import Process
 from time import sleep
 import pyglet
 
 import functions as fs
-# import playNotes as pn
-import playThread as pT
+import playNotes as pn
 
-path = "C:/DevSpace/capstone2/images/"  # 파일 경로
-image_name = "music_test.jpg"       # 이미지 파일 이름
+
+path = "C:/Users/ghwt0504/Desktop/"  # 파일 경로
+image_name = "music.jpg"       # 이미지 파일 이름
 
 note_list = []  # 30노트중 해당 음이 있는자리가 몇번째인지 확인하기 위한 리스트, 자료형은 int
 play_notes = [] # 음을 다중 실행 하기 위한 쓰레드를 저장하기 위한 리스트.
 
-CUTLINE = 4 # 이미지에서 몇줄을 끊을건지
+CUTLINE = 15 # 이미지에서 몇줄을 끊을건지
 
 music_pic = cv2.imread(path + image_name)
 music_pic = cv2.resize(music_pic,dsize=(420,400),interpolation=cv2.INTER_CUBIC)
@@ -38,6 +30,9 @@ if music_pic is not None:
     if cv2.waitKey(0) == ord('c'):
         img_list, rec = fs.sliceImg(music_pic,CUTLINE)
 
+cv2.imshow("cc",rec)
+cv2.waitKey(1)
+
 print(len(img_list))
 
 
@@ -49,7 +44,9 @@ print(img)
 finalPosition = fs.checkPosition(img,CUTLINE)
 print("Final position: ", finalPosition)
 
-pT.playMusic(finalPosition)
+pn.ThreadMaker.playMusic(finalPosition)
+
+
 
 cv2.waitKey()
 cv2.destroyAllWindows()
